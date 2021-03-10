e87db34e5c9253d81ecb2b6fc494bf269029a0c3
hanbin9775 <dnffkrktyd@gmail.com>
2021-03-10 22:30:20 +0900

    sapper project with degit

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,30 +1,20 @@
 <script>
-	export let name;
+  import KeyedEachBlock from "./logic/KeyedEachBlock.svelte";
+  let things = [
+    { id: 1, color: "darkblue" },
+    { id: 2, color: "indigo" },
+    { id: 3, color: "deeppink" },
+    { id: 4, color: "salmon" },
+    { id: 5, color: "gold" },
+  ];
+
+  function handleClick() {
+    things = things.slice(1);
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<button on:click={handleClick}> Remove first thing </button>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+{#each things as thing (thing)}
+  <KeyedEachBlock current={thing.color} />
+{/each}
