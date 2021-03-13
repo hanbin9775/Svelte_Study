8186d6a344eb1463b89f1262ee1455465bdbba75
hanbin9775 <dnffkrktyd@gmail.com>
2021-03-13 21:14:56 +0900

    [#1] study 페이지에서 접근할 수 있는 study/add 페이지구현. 앞으로 svelte에 대해 공부한 내용들 정리해서 하위페이지로 만들 예정

---
+++ sapper-app/src/routes/study/add.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <title>Add</title>
+</svelte:head>
+
+<h1>Add</h1>
+
+<h1>what'sup</h1>
--- sapper-app/src/routes/study/index.svelte
+++ sapper-app/src/routes/study/index.svelte
@@ -1,11 +1,30 @@
 <script context="module">
+  export function preload() {
+    return this.fetch(`study.json`)
+      .then((r) => r.json())
+      .then((posts) => {
+        console.log(posts);
+        return { posts };
+      });
+  }
 </script>
 
-<style>
-</style>
+<script>
+  export let posts;
+</script>
 
 <svelte:head>
   <title>Study</title>
 </svelte:head>
 
 <h1>Svelte Study</h1>
+
+<ul>
+  {#each posts as post}
+    <!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+    <li><a rel="prefetch" href="study/{post.slug}">{post.title}</a></li>
+  {/each}
+</ul>
