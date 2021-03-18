a3e28eb3fb4c078e90288d42b191adbe44885f60
hanbin9775 <dnffkrktyd@gmail.com>
2021-03-18 21:23:04 +0900

    [#2] Svelte Nested Component

--- sapper-app/src/routes/study/_posts.js
+++ sapper-app/src/routes/study/_posts.js
@@ -37,6 +37,35 @@ const posts = [
     memo: `style 태그 내에서 선택자에 대한 스타일 선언. css 문법과 동일. </br>
     각 svelte 파일에 대한 css 선언은 scoped 속성을 가지기 때문에 해당 파일의 컴퍼넌트에만 영향을 끼친다.`,
   },
+  {
+    title: "Nested Component",
+    slug: "nested-component",
+    code: `<pre>
+    //Nested.svelte
+    &lt;p&gt;Nested Paragraph&lt;/p&gt;
+
+    ========================================
+
+    //App.svelte
+    &lt;script&gt; 
+      import Nested from './Nested.svelte'
+    &lt;/script&gt; 
+     
+    &lt;style&gt; 
+      p {
+        color: red;
+      }
+    &lt;/style&gt; 
+    
+    &lt;p&gt;Hello Paragraph&lt;/p&gt;
+    &lt;Nested/&gt;
+    </pre>`,
+    result: `<p style="color: red">Hello Paragraph</p>
+    <p>Nested Paragraph</p>`,
+    memo: `컴포넌트를 분리해서 작성하고 가져오는 방법. script 태그에서 import로 불러오고 사용하면 된다. </br>
+    이렇게 가져온 컴포넌트는 style이 겹치지 않는다. Hello Paragraph 만 color: red; 가 적용된 것을 볼 수 있다.
+    `,
+  },
 ];
 
 posts.forEach((post) => {
